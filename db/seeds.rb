f118df0e6681f39e940b4dc5ce54983a0e6f8589
Amin Mallol <aminm-34@hotmail.fr>
2021-11-12 12:14:15 +0000

    Bundler fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  puts "START"
+  Movie.create(
+    title: Faker::Movie.title,
+    overview: Faker::Quote.famous_last_words,
+    poster_url: 'https://images.unsplash.com/photo-1517604931442-7e0c8ed2963c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80',
+    rating: rand(0.0..10.0)
+  )
+  puts "END"
+end
